19fdbfcfd28931b239c9a2a604fd1d935a186907
hengyulee <hengyuli901229@gmail.com>
2025-07-02 14:16:56 +0800

    Create POST entry point for Notion update

--- index.js
+++ index.js
@@ -3,8 +3,40 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Root endpoint.");
+});
+
+app.post("/update-notion", (req, res) => {
+  let data = req.body;
+
+  if (!Array.isArray(data)) {
+    if (typeof data === "object" && data !== null) {
+      data = [data];
+    } else {
+      return res
+        .status(400)
+        .json({ error: "Expected an array of data in the body request." });
+    }
+  }
+
+  const invalidRows = data.filter((row) => !row.school || !row.program);
+  if (invalidRows.length > 0) {
+    return res.status(400).json({
+      error:
+        'Invalid data format. Each row must contain "school" and "program" fields.',
+      invalid: invalidRows,
+    });
+  }
+
+  res.status(200).json({
+    message: `Updated ${data.length} ${
+      data.length > 1 ? "rows" : "row"
+    } successfully.`,
+    data: data,
+  });
 });
 
 app.listen(PORT, () => {
